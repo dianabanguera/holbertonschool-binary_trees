60acd90c199ac469616bd5bb78f2356eb1bd1629
dianabanguera <dianabangueravillalobo@gmail.com>
2022-07-25 14:06:33 -0500

    inserts a node as the right-child of another node

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: pointer to the node to insert the left-child in
+ * @value: the value to store in the nodo_new node
+ * Return: pointer to the created node,
+ * or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *nodo_new;
+
+	if (!(parent))
+		return (NULL);
+	nodo_new = malloc(sizeof(binary_tree_t));
+	if (!(nodo_new))
+		return (NULL);
+	nodo_new->n = value;
+	nodo_new->parent = parent;
+	nodo_new->left = NULL;
+	nodo_new->right = NULL;
+
+	if (!(parent->right))
+		parent->right = nodo_new;
+	else
+	{
+		parent->right->parent = nodo_new;
+		nodo_new->right = parent->right;
+		parent->right = nodo_new;
+	}
+
+	return (nodo_new);
+}
